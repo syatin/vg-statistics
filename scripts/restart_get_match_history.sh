2502333c3e3b04c5edbadfb79a3e5674c7efe49e
syatin <me@syat.in>
2018-10-05 00:36:26 +0900

    updated restart shell batch

--- scripts/restart_get_match_history.sh
+++ scripts/restart_get_match_history.sh
@@ -5,10 +5,12 @@ CURRENT_DIR=`echo $(cd $(dirname $0) && pwd)`
 BASE_DIR="$CURRENT_DIR/.."
 
 if [ -e $BASE_DIR/3v3_$REGION.pid ]; then
-    cat $BASE_DIR/3v3_$REGION.pid & kill $!
+    kill `cat $BASE_DIR/3v3_$REGION.pid`
+    rm -f $BASE_DIR/3v3_$REGION.pid
 fi
 if [ -e $BASE_DIR/5v5_$REGION.pid ]; then
-    cat $BASE_DIR/5v5_$REGION.pid & kill $!
+    kill `cat $BASE_DIR/5v5_$REGION.pid`
+    rm -f $BASE_DIR/5v5_$REGION.pid
 fi
 
 nohup python $BASE_DIR/get_match_history.py ranked $REGION >> $BASE_DIR/log/get_match_history_3v3_$REGION.log & echo $! > $BASE_DIR/3v3_$REGION.pid
